74e503a4aeead52d300456eb26ef2826eee5c7b1
Bobby Shaw <shawuk2006@gmail.com>
2026-07-02 22:51:19 +0100

    function to retrieve refresh token from json file

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const CLIENT_ID = 208780
 const CLIENT_SECRET = 'c694420fe9b758104c4c07bd20a13c019bc98395'
-let refresh_token = ''
 
-async function getRefreshToken() {
-    const code = 'cb75401ba497c2e56d9dfc63f561c5de4c24b493'
-    const url = `https://www.strava.com/oauth/token?client_id${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=`
-    const response = fetch()
+async function retrieveRefreshToken() {
+  try {
+    const response = await fetch('./info.json')
+      .then(response => response.json())
+      .then(data => data)
+      .catch(error => console.error('Error fetching JSON:', error));
+    
+    if (typeof response.refresh_token === "string" && response.refresh_token.length === 0) {
+      console.log("You are not authorised")
+    }
+  } catch(err) {
+    console.log("Something wrong has occured: " + err)
+  }
 }
-
 
 async function getStats() {
     const ACCESS_TOKEN = "b5ae7f111f9d1bbd8f85dabf259f1cdc90a317f8"
@@ -25,4 +32,8 @@ async function getStats() {
     console.log(activities)
 }
 
-getStats()
+document.getElementById('getActivity-btn').addEventListener('click', function() {
+
+})
+
+retrieveRefreshToken()
