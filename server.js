12de0969b73b5191ceaf3ae97cd36692964cb422
Bobby Shaw <shawuk2006@gmail.com>
2026-07-03 15:10:25 +0100

    server now able to exchange code for refresh token

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const port = 3000
 app.use(express.static("public"));
 app.use(express.json());
 
+const CLIENT_ID = 208780
+const CLIENT_SECRET = 'c694420fe9b758104c4c07bd20a13c019bc98395'
+
 app.listen(port, (err) => {
     if (err) {
         console.log("Error: "+ err)
@@ -16,19 +19,33 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html")
 })
 
-app.get("/exchange_token", (req, res) => {
+app.get("/exchange_token", async(req, res) => {
     // gets code from url
     const { returnCode } = require('./returnCode')
-    const url = req.protocol + '://' + req.get('host') + req.originalUrl;
+    let url = req.protocol + '://' + req.get('host') + req.originalUrl;
     
     const code = returnCode(url)
     if (code === null) {
         console.log("Error - Could not find code")
     } else {
-        console.log(code)
+        console.log("Code: " + code)
     }
 
     // exchanges code for refresh token
-    
+    url = `https://www.strava.com/oauth/token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${code}&grant_type=authorization_code`
+    const response = await fetch(url, {
+        method: "POST",
+    })
+    .then(response => response.json())
+    .then(data => data)
+    .catch(err => console.log(err))
+
+    let refresh_token = ''
+    if (!('refresh_token' in response)) {
+        console.log("Error - Could not read refresh token")
+    } else {
+        refresh_token = response.refresh_token
+        console.log("Refresh Token: " + refresh_token)
+    }
 
 })
