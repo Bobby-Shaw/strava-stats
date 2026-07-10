708b81749e8478f3e00e1445e17460f6b21b2c83
Bobby Shaw <shawuk2006@gmail.com>
2026-07-10 15:57:31 +0100

    Created a card for webpage that displays some activity data. Also added some styling

--- public/activites.js
+++ public/activites.js
@@ -1,10 +1,33 @@
+function displayData(response) {
+    console.log(JSON.stringify(response, null, 2))
+
+    const kmFormatter = new Intl.NumberFormat('en-US', {
+        unit: 'kilometer',
+        unitDisplay: 'short',
+        maximumFractionDigits: 2,
+        minimumFractionDigits: 2
+    });
+
+    let html = `
+    <div id="results-card">
+        <h1 id="title">${response.name}</h3>    
+        <div id="horizontal_stats">
+            <p><span class="label">Distance:</span> <br> ${kmFormatter.format(response.distance / 1000 )}km</p>
+            <p><span class="label">Time: </span> <br> ${Math.round(Number(response.elapsed_time) / 60)} mins</p>
+        </div>
+    </div>
+    `
+
+    document.getElementById("lastRunData-div").innerHTML = html
+}
+
 document.getElementById('getLastRun-btn').addEventListener('click', async function() {
     const response = await fetch('/last_run', {
-        method: "POST",
+        method: "GET",
     })
     .then(response => response.json())
     .then(data => data)
     .catch(err => console.error(err))
 
-    document.getElementById("lastRunData-div").innerText = JSON.stringify(response[0], null, 2)
+    displayData(response[0])
 })
