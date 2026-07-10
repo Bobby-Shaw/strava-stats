341fd0901c75257d3f874e9d040ad43d1db456b9
Bobby Shaw <shawuk2006@gmail.com>
2026-07-10 20:57:53 +0100

    Includes an overivew of all selected runs, including averages

--- public/activities.js
+++ public/activities.js
@@ -1,28 +1,88 @@
 function displayRuns(runs) {
 
-    const kmFormatter = new Intl.NumberFormat('en-US', {
-        unit: 'kilometer',
-        unitDisplay: 'short',
+    let totalDistance = 0
+    let totalHeartRate = 0
+    let totalTime = 0
+    let numberOfRuns = 0
+    let heartRateCount = 0
+
+    const numberFormatter = new Intl.NumberFormat('en-US', {
         maximumFractionDigits: 2,
         minimumFractionDigits: 2
-    });
+    })
+
+    const wholeNumberFormatter = new Intl.NumberFormat('en-US', {
+        maximumFractionDigits: 0
+    })
+
+    function formatDistance(meters) {
+        return `${numberFormatter.format(Number(meters) / 1000)} km`
+    }
+
+    function formatTime(seconds) {
+        return `${wholeNumberFormatter.format(Number(seconds) / 60)} mins`
+    }
+
+    function formatHeartRate(heartRate) {
+        if (!Number.isFinite(Number(heartRate))) {
+            return 'N/A'
+        }
+
+        return `${wholeNumberFormatter.format(Number(heartRate))} bpm`
+    }
 
     let html = ``
     for (let i = 0; i < runs.length; i++) {
-        html += `
-            <div class="run-card">
-                <h1 class="run-title">${runs[i].name}</h1>
-                <div class="run-stats">
-                    <p><span class="label">Distance:</span> <br> ${kmFormatter.format(runs[i].distance / 1000 )} km</p>
-                    <p><span class="label">Time: </span> <br> ${Math.round(Number(runs[i].elapsed_time) / 60)} mins</p>
-                    <p><span class="label">Avg Heart Rate: </span> <br> ${runs[i].average_heartrate} bpm</p> 
+
+        if (runs[i].type == "Run") {
+            numberOfRuns += 1
+            html += `
+                <div class="run-card">
+                    <h1 class="run-title">${runs[i].name}</h1>
+                    <div class="run-stats">
+                        <p><span class="label">Distance:</span> <br> ${formatDistance(runs[i].distance)}</p>
+                        <p><span class="label">Time: </span> <br> ${formatTime(runs[i].elapsed_time)}</p>
+                        <p><span class="label">Avg Heart Rate: </span> <br> ${formatHeartRate(runs[i].average_heartrate)}</p> 
+                    </div>
                 </div>
-            </div>
-        `
+            `
+
+            totalDistance += Number(runs[i].distance)
+            totalTime += Number(runs[i].elapsed_time)
+
+            if (Number.isFinite(Number(runs[i].average_heartrate))) {
+                totalHeartRate += Number(runs[i].average_heartrate)
+                heartRateCount += 1
+            }
+        }
     }
 
 
     document.getElementById("runsData-div").innerHTML = html
+
+    if (numberOfRuns == 0) {
+        document.getElementById('runsOverview').innerHTML = `
+            <div class="run-card" id="overview-div">
+                <p>No runs found.</p>
+            </div>
+        `
+        return
+    }
+
+    let averageDistance = totalDistance / numberOfRuns
+    let averageHeartRate = heartRateCount > 0 ? totalHeartRate / heartRateCount : null
+    let averageTime = totalTime / numberOfRuns
+
+    document.getElementById('runsOverview').innerHTML = `
+        <div class="run-card" id="overview-div">
+        <h1 class="run-title">Overview</h1>
+            <div class="run-stats">
+                <p><span class="label">Avg Distance:</span> <br> ${formatDistance(averageDistance)}</p>
+                <p><span class="label">Avg Time:</span> <br> ${formatTime(averageTime)}</p>
+                <p><span class="label">Avg Heart Rate:</span> <br> ${formatHeartRate(averageHeartRate)}</p>
+            </div>
+        </div>
+    `
 }
 
 document.getElementById('getRuns-btn').addEventListener('click', async function() {
