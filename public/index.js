445b8274a76e235f86ff9e971f884f629214d2f8
Bobby Shaw <shawuk2006@gmail.com>
2026-07-11 02:25:46 +0100

    changes redirect_uri to automatically use the current url instead of hard coded value

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
 document.getElementById('authorize-btn').addEventListener('click', function() {
-    window.location.href = "https://www.strava.com/oauth/authorize?client_id=208780&response_type=code&redirect_uri=https://strava-stats-9i47.onrender.com/exchange_token&approval_prompt=force&scope=read,activity:read_all"
+    const redirect_uri = window.location.href + "exchange_token"
+    window.location.href = `https://www.strava.com/oauth/authorize?client_id=208780&response_type=code&redirect_uri=${redirect_uri}&approval_prompt=force&scope=read,activity:read_all`
 })
