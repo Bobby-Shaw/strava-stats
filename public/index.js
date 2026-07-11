3a5183fd8044e7cf07a9b710f9c06ef9aba1c088
Bobby Shaw <104156702+Bobby-Shaw@users.noreply.github.com>
2026-07-11 01:51:19 +0100

    Update redirect URI for Strava OAuth authorization

--- public/index.js
+++ public/index.js
@@ -1,3 +1,3 @@
 document.getElementById('authorize-btn').addEventListener('click', function() {
-    window.location.href = "https://www.strava.com/oauth/authorize?client_id=208780&response_type=code&redirect_uri=http://localhost:3000/exchange_token&approval_prompt=force&scope=read,activity:read_all"
+    window.location.href = "https://www.strava.com/oauth/authorize?client_id=208780&response_type=code&redirect_uri=https://strava-stats-9i47.onrender.com/exchange_token&approval_prompt=force&scope=read,activity:read_all"
 })
